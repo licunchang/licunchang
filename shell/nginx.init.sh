c9caba1e5e19da69a01f4d37adb33b40193ea8be
licunchang <printf@live.com>
2013-04-12 14:58:20 +0800

    USR1

--- shell/nginx.init.sh
+++ shell/nginx.init.sh
@@ -94,8 +94,7 @@ reload() {
 reopen-logs() {
     configtest || return $?
     echo -n $"Re-opening log files: "
-    # changing configuration, keeping up with a changed time zone (only for FreeBSD and Linux), 
-    # starting new worker processes with a new configuration, graceful shutdown of old worker processes
+    # re-opening log files
     killproc $NGINX -USR1
     RETVAL=$?
     echo
